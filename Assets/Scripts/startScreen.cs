264517cf1682bd0e8b2a2ba317fecceac361ede7
murog <guillermina.muro@gmail.com>
2018-01-05 13:51:37 -0800

    added sprites to start screen, camera moves through with adjustable speed

--- Assets/Scripts/startScreen.cs
+++ Assets/Scripts/startScreen.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class startScreen : MonoBehaviour {
 	public float horizontalSpeed = 2.0F;
 	public float verticalSpeed = 2.0F;
+	public float resetSpeed = 1.0F;
+	public float cameraSpeed = 1.5F;
 	private bool gameStart;
 	private Rigidbody rb;
 	private float h;
+	private float v;
 	Quaternion originalRotation;
+	private bool restoreRotation = false;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
@@ -17,17 +21,28 @@ public class startScreen : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update() {
-		if (!gameStart) {
+		// allow for horizontal mouse movement
+		if (!restoreRotation) {
 			h = horizontalSpeed * Input.GetAxis ("Mouse X");
-//		float v = verticalSpeed * Input.GetAxis("Mouse Y");
-			transform.Rotate (0, h, 0);
+			v = verticalSpeed * Input.GetAxis ("Mouse Y");
+			transform.Rotate (v, h, 0);
 		}
 		if (Input.GetMouseButtonDown (0)) {
 			gameStart = true;
-			transform.Rotate (0, -h, 0);
+			restoreRotation = true;
+//			transform.Rotate (0, -h, 0);
 		}
-		if (gameStart) {
-			rb.velocity = new Vector3 (0, 0, 1);
+		if (restoreRotation) {
+			transform.rotation = Quaternion.Lerp(transform.rotation,originalRotation,Time.time * resetSpeed);
+		}
+		if (transform.rotation == originalRotation) {
+			restoreRotation = false;
+		}
+		if ((gameStart) && (transform.position.z < 25)) {
+			rb.velocity = (new Vector3 (0, 0, 1)) * cameraSpeed;
+		}
+		if (transform.position.z > 25) {
+			rb.velocity = new Vector3 (0, 0, 0);
 		}
 	}
 }
